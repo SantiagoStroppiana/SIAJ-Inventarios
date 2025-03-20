0e2f884366588b0fc912145057e7d8c304af96bf
Santiago Stroppiana <santiagostroppiana@outlook.es>
2025-03-20 19:02:56 -0300

    Creacion proyecto en Spring

---
+++ siaj-inventarios/src/main/java/com/siajinventarios/SiajInventariosApplication.java
@@ -0,0 +1,13 @@
+package com.siajinventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiajInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiajInventariosApplication.class, args);
+	}
+
+}
---
+++ siaj-inventarios/src/test/java/com/siajinventarios/SiajInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siajinventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiajInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
