2d19e5122ceb1fd8db9e01d1e1a6aa17d2e217b4
Santiago Stroppiana <santiagostroppiana@outlook.es>
2025-05-08 21:47:22 -0300

    configuracion basica

--- desktop/src/main/java/org/example/desktop/HelloApplication.java
+++ desktop/src/main/java/org/example/desktop/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.backend.BackendServer;
 
 import java.io.IOException;
 
@@ -23,6 +24,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        new Thread(BackendServer::startServer).start();
         launch();
     }
 }
