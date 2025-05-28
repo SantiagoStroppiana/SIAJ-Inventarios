f84c98f10052c5cd0141e12fdd558dd2db401078
Santiago Stroppiana <santiagostroppiana@outlook.es>
2025-05-28 00:49:32 -0300

    Revert "Vista proveedores"

--- backend/src/main/java/siaj/inventarios/controller/ProveedorController.java
+++ backend/src/main/java/siaj/inventarios/controller/ProveedorController.java
@@ -4,8 +4,6 @@ import siaj.inventarios.dto.MensajesResultados;
 import siaj.inventarios.model.Proveedor;
 import siaj.inventarios.service.ProveedorService;
 
-import java.util.List;
-
 public class ProveedorController {
 
     private ProveedorService proveedorService;
@@ -16,10 +14,5 @@ public class ProveedorController {
         return proveedorService.registrarProveedor(proveedor);}
 
     public MensajesResultados actualizarProveedor(Proveedor proveedor) {return proveedorService.actualizarProveedor(proveedor);}
-
-    public List<Proveedor> mostrarProveedores(){
-
-        return proveedorService.listarProveedores();
-    }
 
 }
