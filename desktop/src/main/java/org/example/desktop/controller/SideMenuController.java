31ff00376b120e88da5c29bbe3ca766b0b99c413
Santiago Stroppiana <santiagostroppiana@outlook.es>
2025-06-04 22:47:43 -0300

    controlador del menu lateral

---
+++ desktop/src/main/java/org/example/desktop/controller/SideMenuController.java
@@ -0,0 +1,37 @@
+package org.example.desktop.controller;
+
+import javafx.event.ActionEvent;
+import org.example.desktop.util.StageManager;
+
+public class SideMenuController {
+
+
+    public void irMenuPrincipal(ActionEvent actionEvent) {
+        StageManager.loadScene("/org/example/desktop/menu-view.fxml", 1200, 800);
+    }
+
+    public void irProductos(ActionEvent actionEvent){
+        StageManager.loadScene("/org/example/desktop/productos-view.fxml", 1200, 800);
+    }
+
+    public void irProveedores(ActionEvent actionEvent){
+        StageManager.loadScene("/org/example/desktop/proveedores-view.fxml", 1200, 800);
+    }
+
+    public void irUsuarios(ActionEvent actionEvent){
+        StageManager.loadScene("/org/example/desktop/usuarios-view.fxml", 1200, 800);
+    }
+
+    public void irReportes(ActionEvent actionEvent){
+        StageManager.loadScene("/org/example/desktop/menu-view.fxml", 1200, 800);
+    }
+
+    public void irPuntoVenta(ActionEvent actionEvent){
+        StageManager.loadScene("/org/example/desktop/menu-view.fxml", 1200, 800);
+    }
+
+    public void logout(ActionEvent actionEvent){
+
+    }
+
+}
