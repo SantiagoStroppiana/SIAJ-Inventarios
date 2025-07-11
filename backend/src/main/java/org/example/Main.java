982339015614641e5155316c6ae6f5dae7b08cd4
Santiago Stroppiana <santiagostroppiana@outlook.es>
2025-07-11 12:03:10 -0300

    Otro fix en main, docker, pom y railway.toml

--- backend/src/main/java/org/example/Main.java
+++ backend/src/main/java/org/example/Main.java
@@ -3,7 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        BackendServer.iniciar();
+        try{
+            BackendServer.iniciar();
+        }catch (Exception e){
+            System.err.println(" Error al iniciar el servidor: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
 
     }
 }
